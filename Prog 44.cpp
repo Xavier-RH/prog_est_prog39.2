5e0a213c323d2578834a62300d7f19538475062b
Xavier Romero Hernández <100869394+Xavier-RH@users.noreply.github.com>
2022-05-22 16:37:03 -0500

    Rename Prog 39.2.cpp to Prog 44.cpp

--- Prog 44.cpp
+++ Prog 44.cpp
@@ -1,4 +1,4 @@
-/* Autor: Xavier Romero Hern·ndez, Realizado: 18/04/2022 
+/* Autor: Xavier Romero Hern√°ndez, Realizado: 18/04/2022 
 	Programa que almacena las calificaciones de un alumno y su faltas para un grupo de 10 alumnos
 	El programa tendra las siguientes opciones:
 		0. Salir
@@ -9,8 +9,8 @@
 		5. Promedio del grupo
 		6. Porcentaje de alumnos Aprobados
 		//Para hacer operaciones con las calificaciones de los alumnos se siguen las siguientes reglas
-			Si el numero de faltas es <=6, la calificaciÛn es la que esta almacenada en el dato promedio de la estructura
-            	Pero si no, la calificaciÛn que se toma es 5
+			Si el numero de faltas es <=6, la calificaci√≥n es la que esta almacenada en el dato promedio de la estructura
+            	Pero si no, la calificaci√≥n que se toma es 5
 	*/
 	
 #include <stdio.h>
